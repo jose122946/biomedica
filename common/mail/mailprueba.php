0bd5dc752027093b69c1f5bbbaf6f5b100d5e767
jose122946 <jose122946@hotmail.com>
2015-11-06 22:07:02 -0600

    se agrega el mail

---
+++ common/mail/mailprueba.php
@@ -0,0 +1,14 @@
+<?
+use yii\helpers\Html;
+?>
+<div class="password-reset">
+    <p>Hola <?= Html::encode($user->username) ?>,</p>
+
+    <p>Su solicitud del equipo ha sido aceptada,</p>
+    <p>Ahora entrará al diagnóstico</p>
+    <p>en breve recibirá un correo con los datos de la reparación</p>
+    <p>o entre ingrese al sistema</p>
+	
+
+    <p>Que tengá un buen día</p>
+</div>
